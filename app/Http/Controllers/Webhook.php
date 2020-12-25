c1de458b0b290340584ebaa566f9cd1e00a74190
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2020-12-26 01:11:53 +0700

    membuat method invoke() yang digunakan untuk menyimpan log signature dari setiap user yang mengkases aplikasi bot

---
+++ app/Http/Controllers/Webhook.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Gateway\EventLogGateway;
+use App\Gateway\QuestionGateway;
+use App\Gateway\UserGateway;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Log\Logger;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+
+class Webhook extends Controller 
+{
+    /** 
+     * @var LINEBot
+     */
+    private $bot;
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var Response
+     */
+    private $response;
+    /**
+     * @var Logger
+     */
+    private $logger;    
+
+    public function __construct(
+        Request $request,
+        Response $response,
+        Logger $logger        
+    ) {
+        $this->request = $request;
+        $this->response = $request;
+        $this->logger = $logger;
+    }
+
+    public function __invoke()
+    {
+        // get request
+        $body = $this->request->all();
+
+        // debugging data
+        $this->logger->debug('Body', $body);
+
+        // save log
+        $signature = $this->request->server('HTTP_X_LINE_SIGNATURE') ?: '-';
+        $this->logGateway->saveLog($signature, json_encode($body, true));
+
+        return $this->handleEvents();
+    }    
+}

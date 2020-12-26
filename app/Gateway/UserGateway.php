52e7cfb7bb4a9304897a00099c5c504886624fb2
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2020-12-27 03:13:15 +0700

    membuat class UserGateway.php yang bertugas menangani segala macam event yang berhubungan dengan tabel users

---
+++ app/Gateway/UserGateway.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Gateway;
+
+use Illuminate\Database\ConnectionInterface;
+
+class UserGateway
+{
+    /**
+     * @var ConnectionInterface
+     */
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = app('db');
+    }
+
+    // users 
+    public function getUser(string $userId)
+    {
+        $user = $this->db->table('users')->where('user_id', $userId)->first();
+
+        if ($user) 
+        {
+            return (array) $user;
+        }
+
+        return null;
+    }
+
+    public function saveUser(string $userId, string $displayName)
+    {
+        $thin->db->table('user')->insert([
+            'user_id' => $userId,
+            'display_name' => $displayName
+        ]);
+    }
+
+    function setUserProgress(string $userId, int $questionNumer)
+    {
+       
+    }
+
+    function setScore(string $userId, int $score)
+    {
+        $this->db->table('users')->update([
+            'score' => $score,
+            'user_id' => $userId
+        ]);
+    }
+}
